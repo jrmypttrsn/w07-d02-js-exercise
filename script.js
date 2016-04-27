e8b392bcaa46d7072dd1cacd3c8443c7030d2dbb
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-04-26 19:13:19 -0500

    Can change purple to black

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
+function addClass(element, className) {
+  element.classList.add(className);
+}
+
+function removeClass(element, className) {
+  element.classList.remove(className);
+}
+
+// function makePurple() {
+// 	var p = document.getElementsByClassName("list-item")
+// 	if(p.style.color == '#000')
+// 		p.style.color = '#FF9800';
+// 	else {
+// 		p.style.color = '#000';
+// 	}
+// }
+
+function makePurple(p) {
+  if (p.classList.contains("purple")){
+  	removeClass(p, "purple");
+  }
+  else {
+  	addClass(p, "purple");
+  }
+}
+
 function makeOrange() {
   var listItems = document.getElementsByClassName("list-item");
   for (var i = 0; i < listItems.length; i++) {
     addClass(listItems[i], "orange");
   }
 }
+
 
 function returnToBlack() {
   var listItems = document.getElementsByClassName("list-item");
@@ -13,6 +40,19 @@ function returnToBlack() {
 }
 
 
-function addClass(element, className) {
-  element.classList.add(className);
+function hideParagraph() {
+	var h = document.getElementById("pone");
+	if(h.style.display == 'block'){
+	  h.style.display = 'none';
+	}
+	else {
+	  h.style.display = 'block';
+	}
+}
+
+function revealParagraph() {
+	var r = document.getElementById("pone");
+	if(r.style.display == 'none') {
+	  r.style.display = 'block';
+	}
 }
